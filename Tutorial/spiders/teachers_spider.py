a369bec32db00360568953bb466063828587c5ff
Anubhav Srivastava <misteranubhav@gmail.com>
2017-01-02 00:14:11 -0500

    First commit. Working. Depth set to 2. Starting at Lagrange.

---
+++ Tutorial/spiders/teachers_spider.py
@@ -0,0 +1,51 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+	name = "teachers"
+	
+	def start_requests(self):
+		start_url = "https://en.wikipedia.org/wiki/Joseph-Louis_Lagrange"
+		yield scrapy.Request(url=start_url, callback=self.parse)
+
+	def parse(self, response):
+		filename = 'teachers.html'
+		if (response.meta['depth'] < 2):
+			myID = response.meta['myID'] if 'myID' in response.meta else "Joseph-Louis Lagrange"
+			with open(filename, 'a') as f:
+				for pairsel in response.xpath('//tr'):
+					field = pairsel.xpath('th').extract_first()
+					if field is not None:
+						if "student" in field:
+							names = pairsel.xpath('td/a/text()').extract()
+							if names is not None:
+								for name in range(len(names)):
+									yield {
+										"myID": names[name],
+										"relative": myID,
+										"relationship":"student"
+									}
+									f.write(names[name] + " was the student of " + myID + "</br>")
+
+									next_page = pairsel.css('td a::attr(href)').extract()[name]
+									if next_page is not None:
+										next_page = response.urljoin(next_page)
+										newmeta = response.meta
+										newmeta['myID'] = names[name]
+										yield scrapy.Request(next_page, callback=self.parse, meta=newmeta)
+						elif "advisor" in field:
+							names = pairsel.xpath('td/a/text()').extract()
+							if names is not None:
+								for name in range(len(names)):
+									yield {
+										"myID": names[name],
+										"relative": myID,
+										"relationship":"advisor"
+									}
+									f.write(names[name] + " was the advisor of " + myID + "</br>")
+
+									next_page = pairsel.css('td a::attr(href)').extract()[name]
+									if next_page is not None:
+										next_page = response.urljoin(next_page)
+										newmeta = response.meta
+										newmeta['myID'] = names[name]
+										yield scrapy.Request(next_page, callback=self.parse, meta=newmeta)
